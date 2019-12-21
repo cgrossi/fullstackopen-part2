76d464aaa1a729e7c06cff2b99d71d4a30170049
cgrossi <cgrossi@gmail.com>
2019-12-20 21:12:46 -0700

    completed exercise 2.13

--- dataforcountries/src/components/App.js
+++ dataforcountries/src/components/App.js
@@ -6,8 +6,13 @@ const App = () => {
   const [query, setQuery] = useState('')
 
   const filtered = countries.filter(country => country.name.includes(query))
-
-  const countryEl = filtered.map(country => <div key={country.numericCode}>{country.name}</div>)
+  const countryEl = filtered.map(country => {
+    return (
+    <div key={country.numericCode}>
+      {country.name} <button onClick={() => setQuery(country.name)}>Show</button>
+    </div>
+    )
+  })
 
   useEffect(() => {
     axios
@@ -28,6 +33,9 @@ const App = () => {
       return <div>Too many results. Be more specific.</div>
     } else if (filtered.length === 1) {
       const [ country ]  = filtered
+      axios.get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`).then(response => {
+        
+      })
       return (
         <div>
           <h1>{country.name}</h1>
@@ -36,6 +44,8 @@ const App = () => {
           <h2>Languages</h2>
           <ul>{country.languages.map(({iso639_1, name}) => <li key={iso639_1}>{name}</li>)}</ul>
           <div><img src={country.flag} alt={`${country.name} - flag`} width="180px" /></div>
+          <h3>Weather in {country.capital}</h3>
+          <strong>Temperature: </strong>
         </div>
       )
     } else {
